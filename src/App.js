b2b5b4f9e659024f988d65f10f9eca47570281df
Koray KOKSAL <tkkoksal@icloud.com>
2023-07-03 21:23:17 +0300

    data parse change

--- src/App.js
+++ src/App.js
@@ -12,10 +12,23 @@ function App() {
     <>
 
       {/* jsx içinde js kodu yazılacak sa süslü içinde belirtmek gerekiyor */}
+
+      {/* tek satırlık işlem yani single statement işlemi yapılacaksa normal parantez içinde yapılır. */}
       
-      {data.map(({ id, language, img, btnName }) => (
+      
+      {/* {data.map(({ id, language, img, btnName }) => (
         <Card key={id} lang={language} img={img} btn={btnName} />
-      ))}
+      ))} */}
+
+
+      {/* birden fazla satırlı işlem yapılacaksa return ifadesi ile işlem yapılmalıdır */}
+      {data.map((item) =>{
+
+        const { id, language, img, btnName } = item
+
+        return <Card key={id} lang={language} img={img} btn={btnName} />
+
+      })}
 
 
       
